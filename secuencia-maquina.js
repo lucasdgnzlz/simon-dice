05c8c005ac8645e0b827dbfba2737752a96344cc
Lucas <lucasdgonzalez@outlook.com>
2023-02-17 21:20:36 -0300

    Modifica los nombres de variables y constantes
    El nombre anterior no dejaba en claro que fueran elementos de html, se les agregó un $ para diferenciarlos.

--- secuencia-maquina.js
+++ secuencia-maquina.js
@@ -12,16 +12,16 @@ function seleccionarColorMaquina(){
     let colorMaquina;
     
     if(numeroAleatorio === 0){
-        return colorMaquina = coloresTablero.colorVerde;
+        return colorMaquina = coloresTablero.$colorVerde;
     }
     else if(numeroAleatorio === 1){
-        return colorMaquina = coloresTablero.colorRojo;
+        return colorMaquina = coloresTablero.$colorRojo;
     }
     else if(numeroAleatorio === 2){
-        return colorMaquina = coloresTablero.colorAmarillo;
+        return colorMaquina = coloresTablero.$colorAmarillo;
     }
     else if(numeroAleatorio === 3){
-        return colorMaquina = coloresTablero.colorAzul;
+        return colorMaquina = coloresTablero.$colorAzul;
     }
     else{
         return false;
@@ -32,60 +32,60 @@ function guardarColorMaquinaEnLista(colorElegido){
     listaSecuenciaMaquina.push(colorElegido);
 }
 
-function mostrarColorElegidoMaquina($colorElegido){
-    if($colorElegido === coloresTablero.colorVerde){
-        $colorElegido.id = "mostrar-verde-iluminado";
+function mostrarColorElegidoMaquina(colorElegido){
+    if(colorElegido === coloresTablero.$colorVerde){
+        colorElegido.id = "mostrar-verde-iluminado";
     }
-    if($colorElegido === coloresTablero.colorRojo){
-        $colorElegido.id = "mostrar-rojo-iluminado";
+    if(colorElegido === coloresTablero.$colorRojo){
+        colorElegido.id = "mostrar-rojo-iluminado";
     }
-    if($colorElegido === coloresTablero.colorAmarillo){
-        $colorElegido.id = "mostrar-amarillo-iluminado";
+    if(colorElegido === coloresTablero.$colorAmarillo){
+        colorElegido.id = "mostrar-amarillo-iluminado";
     }
-    if($colorElegido === coloresTablero.colorAzul){
-        $colorElegido.id = "mostrar-azul-iluminado";
+    if(colorElegido === coloresTablero.$colorAzul){
+        colorElegido.id = "mostrar-azul-iluminado";
     }
 
     setTimeout(function(){
-        $colorElegido.id = "";
+        colorElegido.id = "";
     }, 500);
 }
 
 function manejarColoresElegidosMaquina(){
     for(let i = 0; i < listaSecuenciaMaquina.length; i++){
-        if(listaSecuenciaMaquina[i] === coloresTablero.colorVerde){
+        if(listaSecuenciaMaquina[i] === coloresTablero.$colorVerde){
             listaSecuenciaMaquina.forEach(function(){
                 const retrasoEnMilisegundos = (i + 1) * 1000;
 
                 setTimeout(function(){
-                    mostrarColorElegidoMaquina(coloresTablero.colorVerde, i);
+                    mostrarColorElegidoMaquina(coloresTablero.$colorVerde, i);
                 }, retrasoEnMilisegundos)
             })
         }
-        else if(listaSecuenciaMaquina[i] === coloresTablero.colorRojo){
+        else if(listaSecuenciaMaquina[i] === coloresTablero.$colorRojo){
             listaSecuenciaMaquina.forEach(function(){
                 const retrasoEnMilisegundos = (i + 1) * 1000;
 
                 setTimeout(function(){
-                    mostrarColorElegidoMaquina(coloresTablero.colorRojo, i);
+                    mostrarColorElegidoMaquina(coloresTablero.$colorRojo, i);
                 }, retrasoEnMilisegundos)
             })
         }
-        else if(listaSecuenciaMaquina[i] === coloresTablero.colorAmarillo){
+        else if(listaSecuenciaMaquina[i] === coloresTablero.$colorAmarillo){
             listaSecuenciaMaquina.forEach(function(){
                 const retrasoEnMilisegundos = (i + 1) * 1000;
 
                 setTimeout(function(){
-                    mostrarColorElegidoMaquina(coloresTablero.colorAmarillo, i);
+                    mostrarColorElegidoMaquina(coloresTablero.$colorAmarillo, i);
                 }, retrasoEnMilisegundos)
             })
         }
-        else if(listaSecuenciaMaquina[i] === coloresTablero.colorAzul){
+        else if(listaSecuenciaMaquina[i] === coloresTablero.$colorAzul){
             listaSecuenciaMaquina.forEach(function(){
                 const retrasoEnMilisegundos = (i + 1) * 1000;
 
                 setTimeout(function(){
-                    mostrarColorElegidoMaquina(coloresTablero.colorAzul, i);
+                    mostrarColorElegidoMaquina(coloresTablero.$colorAzul, i);
                 }, retrasoEnMilisegundos)
             })
         }
